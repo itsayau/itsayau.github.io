884a33b7180a353a4f21e1519ca4ca138439b435
itsayau <148083185+itsayau@users.noreply.github.com>
2023-10-18 23:14:26 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,710 @@
+window.correct = 0
+window.wrong = 0
+
+window.result = 0
+
+function verifyCorrect1() {
+    var checkBox = document.getElementById("wordCheck1")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect2() {
+    var checkBox = document.getElementById("wordCheck2")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect3() {
+    var checkBox = document.getElementById("wordCheck3")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect4() {
+    var checkBox = document.getElementById("wordCheck4")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect5() {
+    var checkBox = document.getElementById("wordCheck5")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect6() {
+    var checkBox = document.getElementById("wordCheck6")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect7() {
+    var checkBox = document.getElementById("wordCheck7")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect8() {
+    var checkBox = document.getElementById("wordCheck8")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect9() {
+    var checkBox = document.getElementById("wordCheck9")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect10() {
+    var checkBox = document.getElementById("wordCheck10")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect11() {
+    var checkBox = document.getElementById("wordCheck11")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect12() {
+    var checkBox = document.getElementById("wordCheck12")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect13() {
+    var checkBox = document.getElementById("wordCheck13")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect14() {
+    var checkBox = document.getElementById("wordCheck14")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect15() {
+    var checkBox = document.getElementById("wordCheck15")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect16() {
+    var checkBox = document.getElementById("wordCheck16")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect17() {
+    var checkBox = document.getElementById("wordCheck17")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect18() {
+    var checkBox = document.getElementById("wordCheck18")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect19() {
+    var checkBox = document.getElementById("wordCheck19")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect20() {
+    var checkBox = document.getElementById("wordCheck20")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect21() {
+    var checkBox = document.getElementById("wordCheck21")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect22() {
+    var checkBox = document.getElementById("wordCheck22")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect23() {
+    var checkBox = document.getElementById("wordCheck23")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect24() {
+    var checkBox = document.getElementById("wordCheck24")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect25() {
+    var checkBox = document.getElementById("wordCheck25")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect26() {
+    var checkBox = document.getElementById("wordCheck26")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect27() {
+    var checkBox = document.getElementById("wordCheck27")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect28() {
+    var checkBox = document.getElementById("wordCheck28")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect29() {
+    var checkBox = document.getElementById("wordCheck29")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect30() {
+    var checkBox = document.getElementById("wordCheck30")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect31() {
+    var checkBox = document.getElementById("wordCheck31")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect32() {
+    var checkBox = document.getElementById("wordCheck32")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect33() {
+    var checkBox = document.getElementById("wordCheck33")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect34() {
+    var checkBox = document.getElementById("wordCheck34")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect35() {
+    var checkBox = document.getElementById("wordCheck35")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect36() {
+    var checkBox = document.getElementById("wordCheck36")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect37() {
+    var checkBox = document.getElementById("wordCheck37")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect38() {
+    var checkBox = document.getElementById("wordCheck38")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect39() {
+    var checkBox = document.getElementById("wordCheck39")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect40() {
+    var checkBox = document.getElementById("wordCheck40")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect41() {
+    var checkBox = document.getElementById("wordCheck41")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect42() {
+    var checkBox = document.getElementById("wordCheck42")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect43() {
+    var checkBox = document.getElementById("wordCheck43")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect44() {
+    var checkBox = document.getElementById("wordCheck44")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect45() {
+    var checkBox = document.getElementById("wordCheck45")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect46() {
+    var checkBox = document.getElementById("wordCheck46")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect47() {
+    var checkBox = document.getElementById("wordCheck47")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect48() {
+    var checkBox = document.getElementById("wordCheck48")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect49() {
+    var checkBox = document.getElementById("wordCheck49")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect50() {
+    var checkBox = document.getElementById("wordCheck50")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect51() {
+    var checkBox = document.getElementById("wordCheck51")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect52() {
+    var checkBox = document.getElementById("wordCheck52")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect53() {
+    var checkBox = document.getElementById("wordCheck53")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect54() {
+    var checkBox = document.getElementById("wordCheck54")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect55() {
+    var checkBox = document.getElementById("wordCheck55")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect56() {
+    var checkBox = document.getElementById("wordCheck56")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect57() {
+    var checkBox = document.getElementById("wordCheck57")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect58() {
+    var checkBox = document.getElementById("wordCheck58")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect59() {
+    var checkBox = document.getElementById("wordCheck59")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect60() {
+    var checkBox = document.getElementById("wordCheck60")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect61() {
+    var checkBox = document.getElementById("wordCheck61")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect62() {
+    var checkBox = document.getElementById("wordCheck62")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect63() {
+    var checkBox = document.getElementById("wordCheck63")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect64() {
+    var checkBox = document.getElementById("wordCheck64")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect65() {
+    var checkBox = document.getElementById("wordCheck65")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect66() {
+    var checkBox = document.getElementById("wordCheck66")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect67() {
+    var checkBox = document.getElementById("wordCheck67")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect68() {
+    var checkBox = document.getElementById("wordCheck68")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect69() {
+    var checkBox = document.getElementById("wordCheck69")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect70() {
+    var checkBox = document.getElementById("wordCheck70")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect71() {
+    var checkBox = document.getElementById("wordCheck71")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect72() {
+    var checkBox = document.getElementById("wordCheck72")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect73() {
+    var checkBox = document.getElementById("wordCheck73")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect74() {
+    var checkBox = document.getElementById("wordCheck74")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect75() {
+    var checkBox = document.getElementById("wordCheck75")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect76() {
+    var checkBox = document.getElementById("wordCheck76")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect77() {
+    var checkBox = document.getElementById("wordCheck77")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect78() {
+    var checkBox = document.getElementById("wordCheck78")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect79() {
+    var checkBox = document.getElementById("wordCheck79")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect80() {
+    var checkBox = document.getElementById("wordCheck80")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect81() {
+    var checkBox = document.getElementById("wordCheck81")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect82() {
+    var checkBox = document.getElementById("wordCheck82")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect83() {
+    var checkBox = document.getElementById("wordCheck83")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect84() {
+    var checkBox = document.getElementById("wordCheck84")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect85() {
+    var checkBox = document.getElementById("wordCheck85")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect86() {
+    var checkBox = document.getElementById("wordCheck86")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect87() {
+    var checkBox = document.getElementById("wordCheck87")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect88() {
+    var checkBox = document.getElementById("wordCheck88")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect89() {
+    var checkBox = document.getElementById("wordCheck89")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect90() {
+    var checkBox = document.getElementById("wordCheck90")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect91() {
+    var checkBox = document.getElementById("wordCheck91")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect92() {
+    var checkBox = document.getElementById("wordCheck92")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect93() {
+    var checkBox = document.getElementById("wordCheck93")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect94() {
+    var checkBox = document.getElementById("wordCheck94")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect95() {
+    var checkBox = document.getElementById("wordCheck95")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect96() {
+    var checkBox = document.getElementById("wordCheck96")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect97() {
+    var checkBox = document.getElementById("wordCheck97")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect98() {
+    var checkBox = document.getElementById("wordCheck98")
+
+    if (checkBox.checked == true) {
+        window.wrong = window.wrong + 1
+    }
+}
+function verifyCorrect99() {
+    var checkBox = document.getElementById("wordCheck99")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+function verifyCorrect100() {
+    var checkBox = document.getElementById("wordCheck100")
+
+    if (checkBox.checked == true) {
+        window.correct = window.correct + 1
+    }
+}
+
+function calculateResult() {
+    window.result = (window.correct) * 375 - (window.wrong) * 1500
+    alert("Сіздің нәтижеңіз: " + window.result)
+}
